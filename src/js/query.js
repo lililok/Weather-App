d0e90a592adea1e025ca4b79029e3259f28296c1
lililok <eshanu.sofia@yandex.ru>
2024-06-12 18:39:01 +0300

    https changes

--- src/js/query.js
+++ src/js/query.js
@@ -1,6 +1,6 @@
 export async function getWeather(cityQuery) {
   try {
-    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=0058d81df506495b8f3143520240906&q=${cityQuery}&days=4`, { mode: "cors" });
+    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=0058d81df506495b8f3143520240906&q=${cityQuery}&days=4`, { mode: "cors" });
     const data = await response.json();
 
     return data;
