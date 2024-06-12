355ddfde3cb29f6ec49f118e86185e4598d49a10
lililok <eshanu.sofia@yandex.ru>
2024-06-12 20:20:40 +0300

    no more f :(

--- src/js/parse.js
+++ src/js/parse.js
@@ -3,35 +3,7 @@ export function aggParse(object) {
     const parsedLocation = parseLocation(object.location)
     const parsedForecast = parseForecast(object.forecast.forecastday)
 
-    const dataFahrenheit = {
-        current: {
-            temp: parsedCurrent.temp_f+'ºF',
-            conditionText: parsedCurrent.condition_text,
-            conditionIcon: parsedCurrent.condition_icon,
-            feels: parsedCurrent.feelslike_f+'ºF',
-            uvIndex: parsedCurrent.uv_index,
-            wind: parsedCurrent.wind_kph+'KPH',
-            cloud: parsedCurrent.cloud+'%',
-            humidity: parsedCurrent.humidity+'%',
-            sunrise: parsedForecast.sunrise,
-            sunset: parsedForecast.sunset,
-        },
-        location: {
-            name: parsedLocation.city,
-            region: parsedLocation.region,
-            country: parsedLocation.country,
-            time: parsedLocation.time
-        },
-        forecast: {
-            firstWeekday: dateToWeekday(parsedForecast.tomorrow_date),
-            firstTemp: parsedForecast.tomorrow_temp_f+'ºF',
-            secondWeekday: dateToWeekday(parsedForecast.atomorrow_date),
-            secondTemp: parsedForecast.atomorrow_temp_f+'ºF',
-            thirdWeekday: dateToWeekday(parsedForecast.aatomorrow_date),
-            thirdTemp: parsedForecast.aatomorrow_temp_f+'ºF',
-        }
-    }
-    const dataCelsius = {
+    const data = {
         current: {
             temp: parsedCurrent.temp_c+'ºC',
             conditionText: parsedCurrent.condition_text,
@@ -59,22 +31,20 @@ export function aggParse(object) {
             thirdTemp: parsedForecast.aatomorrow_temp_c+'ºC',
         }
     }
-    return [dataCelsius, dataFahrenheit];
+    return data;
 }
 
 function parseCurrent(object) {
     const cloud = object.cloud;
     const feelslike_c = object.feelslike_c;
-    const feelslike_f = object.feelslike_f;
     const humidity = object.humidity;
     const temp_c = object.temp_c;
-    const temp_f = object.temp_f;
     const wind_kph = object.wind_kph
     const uv_index = object.uv
     const condition_text = object.condition.text;
     const condition_icon = object.condition.icon;
 
-    const parsedCurrent = {cloud, feelslike_c, feelslike_f, humidity, temp_c, temp_f, wind_kph, uv_index, condition_text, condition_icon}
+    const parsedCurrent = {cloud, feelslike_c, humidity, temp_c, wind_kph, uv_index, condition_text, condition_icon}
     return parsedCurrent;
 }
 
@@ -95,13 +65,10 @@ function parseLocation(object) {
     const atomorrow_date = object[2].date
     const aatomorrow_date = object[3].date
     const tomorrow_temp_c = object[1].day.avgtemp_c
-    const tomorrow_temp_f = object[1].day.avgtemp_f
     const atomorrow_temp_c = object[2].day.avgtemp_c
-    const atomorrow_temp_f = object[2].day.avgtemp_f
     const aatomorrow_temp_c = object[3].day.avgtemp_c
-    const aatomorrow_temp_f = object[3].day.avgtemp_f
 
-    const parsedForecast = {sunrise, sunset, tomorrow_date, atomorrow_date, aatomorrow_date, tomorrow_temp_c, tomorrow_temp_f, atomorrow_temp_c, atomorrow_temp_f, aatomorrow_temp_c, aatomorrow_temp_f}
+    const parsedForecast = {sunrise, sunset, tomorrow_date, atomorrow_date, aatomorrow_date, tomorrow_temp_c, atomorrow_temp_c, aatomorrow_temp_c}
     return parsedForecast;
 }
 
